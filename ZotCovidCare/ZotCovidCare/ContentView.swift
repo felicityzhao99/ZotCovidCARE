5478549130898dee821f7d13f32b0d04338ae4f3
Susie Liu <susiexiaoqi@gmail.com>
2021-01-27 22:00:08 -0800

    add swift framework

---
+++ ZotCovidCare/ZotCovidCare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZotCovidCare
+//
+//  Created by Susie Liu on 1/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZotCovidCare/ZotCovidCare/ZotCovidCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZotCovidCareApp.swift
+//  ZotCovidCare
+//
+//  Created by Susie Liu on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ZotCovidCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
