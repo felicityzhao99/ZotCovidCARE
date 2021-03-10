4b4bd5aa5623f2bfdb7d5416e7cd9f58f3b0e2b9
Kaining Mao <chrissmao99@gmail.com>
2021-03-10 09:56:07 +0800

    Complete user location and anotations adding process

--- ZotCovidCare/ZotCovidCare/UCIMapController.swift
+++ ZotCovidCare/ZotCovidCare/UCIMapController.swift
@@ -9,24 +9,62 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class UCIMapController: UIViewController {
+class UCIMapController: UIViewController,CLLocationManagerDelegate {
 
     @IBOutlet weak var UCIMap: MKMapView!
+    var config = ["darkmode" : 0, "notification" : 0]
+    let loc_manager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        darkModeInitialization()
 
+        UCIMap.showsUserLocation = true
+        let anotation = MKPointAnnotation()
+        anotation.coordinate = CLLocationCoordinate2D(latitude: 33.640455, longitude: -117.844285)
+        UCIMap.addAnnotation(anotation)
+        
+        let region = MKCoordinateRegion(center: anotation.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        UCIMap.setRegion(region, animated: true)
+    
         // Do any additional setup after loading the view.
     }
-    
+    func darkModeInitialization()
+        {
+            guard let path = Bundle.main.path(forResource: "config", ofType: "json") else {return}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                
+                let data = try Data(contentsOf: url)
+                let jsonData = try JSONSerialization.jsonObject(with: data, options: [])
+                if let dictFromJSON = jsonData as? [String : Int]{
+                    config["darkmode"] = dictFromJSON["darkmode"]
+                    config["notification"] = dictFromJSON["notification"]
+                } else {
+                    print("json convert fail\n")
+                }
+                if config["darkmode"] == 1{
+                    setBlack()
+                }else{
+                    setWhite()
+                }
+                return
+            } catch {}
+        }
+        
+        func setBlack()
+        {
+            if #available(iOS 13.0, *) {
+                self.overrideUserInterfaceStyle = .dark
+            }
+        }
+        
+        func setWhite()
+        {
+            if #available(iOS 13.0, *) {
+                self.overrideUserInterfaceStyle = .light
+            }
+        }
 
 }
